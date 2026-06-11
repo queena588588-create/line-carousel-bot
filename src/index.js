6694ccfbab0bf4478bd9513a0aa92fc1728454ea
queena588588-create <queena.588.588@gmail.com>
2026-06-11 23:19:26 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -81,25 +81,25 @@ async function replyCarousel(replyToken, token) {
         productCard(
           "青春極致防曬乳",
           "SPF50+ 保濕防曬不黏膩",
-          "https://img.shop.com/Image/240000/246200/246290/products/895882809.jpg?plain&size=1600x1600",
+          "https://img.shop.com/Image/240000/246200/246290/skualt/40634.jpg?plain&size=800x800",
           "https://www.google.com"
         ),
         productCard(
           "冰冰衣",
           "降溫＋防曬＋對抗光老化",
-          "https://lh7-rt.googleusercontent.com/formsz/AN7BsVAAwaaiY7qvNA8Y4kWtMylSH1IO7Ynn9PSVp--Rgm7NirW7iK3TKSbt5RHWXBjtMTuP1dHPBweaKXez3c7tzJEup_neUyq1PMqqZ7p4EBBrVNwvMOJJmK6AmRhA2tBB5WBqAiKCgQsirIx_BcUdTNTyckP5DzCirxHG1k95Jq5kcFk-YxGHCdgkN1YcEGYNechgc-EaSzV18ZNfyyr8=w640?key=5XGsa3dXCmjPy5PBVdJVnw",
+          "https://shoplineimg.com/562a466de37ec6fdbb000012/6a0c330a018eb10693cf22bc/800x.webp?source_format=jpg",
           "https://forms.gle/FGgQcesTW8YHfdZN9"
         ),
         productCard(
           "雞排",
           "方便料理，加熱即可享用",
-          "https://scontent-sin2-1.cdninstagram.com/v/t51.82787-15/702677192_18322302067285160_5035159655161140196_n.jpg?stp=dst-jpg_e35_p640x640_sh2.08_tt6&_nc_cat=100&ig_cache_key=MzkwMDM2MDM0NDc1NzczOTM1Ng%3D%3D.3-ccb7-5&ccb=7-5&_nc_sid=58cdad&efg=eyJ2ZW5jb2RlX3RhZyI6IlNUT1JZLnhwaWRzLjEyOTAuc2RyLnJlZ3VsYXJfcGhvdG8uQzMifQ%3D%3D&_nc_ohc=tLQsrUDbZnoQ7kNvwH6br74&_nc_oc=AdpnzWo3H7J2CsZMBVpA0n0dUC-dx7KF66YxigpYdXNrk_WoCEIPa8aiRXsFsLzgfaFA1i4rwr6LJ03dQQiFqeGp&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent-sin2-1.cdninstagram.com&_nc_gid=nlRaAGJhxwgfsXFTcSrMdg&_nc_ss=7a22e&oh=00_Af8BweOUn80mo6JcTWe6-tsvBysUHyD7kjTQHYF_2_i9xg&oe=6A3057BF",
+          "https://drive.google.com/file/d/1x47Jdve6hAgkr4FMO1q-Ybu0lF5lNtpN/view?usp=drive_link",
           "https://forms.gle/sE16zUJx3Tb359qq7"
         ),
         productCard(
           "櫻桃",
           "夏季限定 香甜多汁",
-          "https://scontent-sin2-3.cdninstagram.com/v/t51.82787-15/722995335_18325777477285160_3070691715997546624_n.jpg?stp=dst-jpg_e35_p640x640_sh2.08_tt6&_nc_cat=107&ig_cache_key=MzkxNzEyNDI2NTQ0MzE4NTI4NQ%3D%3D.3-ccb7-5&ccb=7-5&_nc_sid=58cdad&efg=eyJ2ZW5jb2RlX3RhZyI6IlNUT1JZLnhwaWRzLjEyOTAuc2RyLnJlZ3VsYXJfcGhvdG8uQzMifQ%3D%3D&_nc_ohc=l4fp3LgcK2UQ7kNvwHnQJD-&_nc_oc=AdrN0dAG1vAFHf20ogWOneMyh3-rCoU64-mdpKVTDYyjC7uF740E-KPaaKonJ6C1Rug2Picpb2wa3uK4ctq-x6vy&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent-sin2-3.cdninstagram.com&_nc_gid=ku_KdGfOX3nm7hyF15DWhg&_nc_ss=7a22e&oh=00_Af90Z_7ZerZI1Nzs2AAoX0U8k-RjX38HTuXs6FEDlqh0OA&oe=6A305F33",
+          "https://drive.google.com/file/d/1xjr2AUGkAB65yphmFWJcgthKQE5aGpvN/view?usp=drive_link",
           "https://www.google.com"
         )
       ]
