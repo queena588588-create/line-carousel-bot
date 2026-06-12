b0236eaf0403bd7d8c730b82674ac8c8ad7c4658
queena588588-create <queena.588.588@gmail.com>
2026-06-13 05:35:19 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -135,8 +135,7 @@ async function replyPrivateButton(replyToken, token) {
         contents: [
           {
             type: "text",
-            text: "⚡︎ 線上快速解答 
-              偷偷點一下，沒人會發現的 🤫",
+            text: "⚡︎ 偷偷點一下，沒人會發現的 🤫",
             weight: "bold",
             size: "lg"
           }
