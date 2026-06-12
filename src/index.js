36baa2795f6a3cb2884dfc0e4b87fd4f69c1088c
queena588588-create <queena.588.588@gmail.com>
2026-06-13 05:53:31 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -84,7 +84,7 @@ if (text === "牛排") {
 return new Response("OK");
     }
   }
-
+};
 
 async function replySimple(replyToken, token, text) {
   await fetch("https://api.line.me/v2/bot/message/reply", {
