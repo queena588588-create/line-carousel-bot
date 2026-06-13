aecb69eb26fde425294908ef133161b8a71eec81
queena588588-create <queena.588.588@gmail.com>
2026-06-13 14:58:14 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -337,18 +337,17 @@ async function replyText(replyToken, token, text) {
     })
   });
 }
-
 async function replyProduct(replyToken, token, imageUrl, text) {
   await fetch("https://api.line.me/v2/bot/message/reply", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-"Authorization": "Bearer " + token
+      "Authorization": "Bearer " + token
     },
     body: JSON.stringify({
       replyToken,
       messages: [
-               {
+        {
           type: "image",
           originalContentUrl: imageUrl,
           previewImageUrl: imageUrl
@@ -356,10 +355,12 @@ async function replyProduct(replyToken, token, imageUrl, text) {
         {
           type: "text",
           text: text
-             ]
+        }
+      ]
     })
   });
 }
+
 async function replyFolderButton(replyToken, token) {
   await replySimple(
     replyToken,
@@ -367,4 +368,3 @@ async function replyFolderButton(replyToken, token) {
     "💰 抗通膨專區\n\n☂️ 超大傘｜買一送一 $850\n🧺 洗衣膠球｜200顆 $777\n🛏️ 冰淇淋被｜買一送一 $999\n🔋 行動電源｜買一送一 $599\n\n📂 商品影片與照片：\nhttps://drive.google.com/drive/folders/1n0dPszGQg5HEGqj2lxelfgsY7xX5g8M0?usp=sharing"
   );
 }
-};
