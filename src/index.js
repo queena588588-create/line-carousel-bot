a1ce99986569e58c2e3740cc4bd51c46e16d49fe
queena588588-create <queena.588.588@gmail.com>
2026-06-13 08:47:17 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -110,7 +110,7 @@ if (text === "牛排") {
     event.replyToken,
     CHANNEL_ACCESS_TOKEN,
     "https://drive.google.com/file/d/1sk5llgMPrI5LkEn42OO5th5UsAh3taEZ/view",
-    "🥩 加拿大 PRIME 雪花凝脂牛排\n\n4片/320g/包 4包組"
+   text: "🥩 加拿大 PRIME 雪花凝脂牛排\n\n4片/320g/包 4包組"
                
   );
 }
