6b564a185ffc96fc4160ace740ea0e519f096897
queena588588-create <queena.588.588@gmail.com>
2026-06-13 08:42:06 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -327,7 +327,7 @@ async function replyText(replyToken, token, text) {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      "Authorization": `Bearer+{token}`
+      "Authorization": "Bearer " + token
     },
     body: JSON.stringify({
       replyToken,
