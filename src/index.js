3d6b913d57020de5016f71061f38fe12cf87385a
queena588588-create <queena.588.588@gmail.com>
2026-06-16 00:05:54 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -192,7 +192,7 @@ if (text === "洗衣球" || text === "洗衣") {
     "https://github.com/queena588588-create/line-carousel-bot/blob/main/%E6%B4%97%E8%A1%A3%E7%90%83.png?raw=true",
       "https://raw.githubusercontent.com/queena588588-create/line-carousel-bot/refs/heads/main/%E6%B4%97%E8%A1%A3%E7%90%83%E4%BD%BF%E7%94%A8%E6%95%99%E5%AD%B8.jpg"
        ],
-    "三效合一洗衣膠球 ☘️→ 買一送一  $777"
+   "🧺 三效合一洗衣膠球 🍀\n買一送一 $777"
   );
 } 
 
