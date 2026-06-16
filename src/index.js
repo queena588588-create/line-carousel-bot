d96753df4f54f6fa9022c856184818d5c8f692e2
queena588588-create <queena.588.588@gmail.com>
2026-06-16 08:28:11 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -647,7 +647,7 @@ async function findProductFromSheet(keyword, SHEET_ID, SHEET_NAME) {
     const intro = row[3] || "";
     const buyMethod = row[4] || "";
 
-    if (sheetKeyword === keyword || productName.includes(keyword)) {
+  if (sheetKeyword === keyword) {
       return {
         keyword: sheetKeyword,
         photo,
